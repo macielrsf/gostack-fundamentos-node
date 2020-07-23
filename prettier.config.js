2f0550dffa55d8685fd39d16958ebc177dde739f
Maciel Santos <macielrsf@gmail.com>
2020-07-22 21:43:07 -0300

    finished

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'avoid',
+    tabWidth: 4,
 };
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,46 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+    private transactionsRepository: TransactionsRepository;
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
+    constructor(transactionsRepository: TransactionsRepository) {
+        this.transactionsRepository = transactionsRepository;
+    }
 
-  public execute(): Transaction {
-    // TODO
-  }
+    public execute({title, value, type}: Request): Transaction {
+        const balance = this.transactionsRepository.getBalance();
+
+        if (!['income', 'outcome'].includes(type)) {
+            throw Error('Type needs to be income or outcome');
+        }
+
+        if (value <= 0) {
+            throw Error('Value needs to be greater than zero');
+        }
+
+        if (typeof value !== 'number') {
+            throw Error('Value needs to be a number');
+        }
+
+        if (type === 'outcome' && value > balance.total) {
+            throw Error('Insufficient funds');
+        }
+
+        const transaction = this.transactionsRepository.create({
+            title,
+            value,
+            type,
+        });
+
+        return transaction;
+    }
 }
 
 export default CreateTransactionService;
